fde2695db42a65a3c8faa4c394e41c47fcc26b08
fouad lmrini <fouad11.lamrini@gmail.com>
2025-02-26 09:17:18 +0000

    erreur de router

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/index', [ProfileController::class, 'index'])->name('index');
+
 });
 
 
